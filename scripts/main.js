3d46868b5c8d419c4f834c19f505e7dcd58f9ed8
Pritom Paul <pritompaul1920@gmail.com>
2024-09-19 19:22:50 +0600

    before adding 6day forecast

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ navigator.geolocation.getCurrentPosition(
       console.log(latitude, longitude);
 
       var map = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=5863cb59bb064eca989192814241809&q=${latitude},${longitude}`
+        `http://api.weatherapi.com/v1/current.json?key=5863cb59bb064eca989192814241809&q=${latitude},${longitude}&aqi=no`
       );
 
       var userdata = await map.json();
@@ -16,10 +16,13 @@ navigator.geolocation.getCurrentPosition(
       let cityName = document.getElementById("city-name");
       let weatherStat = document.getElementById("weather-main");
       let currentTemp = document.getElementById("temp");
+      //   let weatherIcon = document.getElementById("weather-icon1");
 
       cityName.innerHTML = userdata.location.name;
       weatherStat.innerHTML = userdata.current.condition.text;
-      currentTemp.innerHTML = userdata.current.temp_c + "°";
+      currentTemp.innerHTML = Math.round(userdata.current.temp_c) + "°";
+      //   weatherIcon.src = userdata.current.condition.icon;
+      
       // todo: Complete the rest of the data.
     } catch (err) {
       console.error("An error occurred:", err);
